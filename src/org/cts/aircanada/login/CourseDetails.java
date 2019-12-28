bda9eba9a01e378487656018f4463370b2c92216
greensTech67 <greensseleniumtask@gmail.com>
2019-12-28 16:33:41 +0530

    SecondPush

--- src/org/cts/aircanada/login/CourseDetails.java
+++ src/org/cts/aircanada/login/CourseDetails.java
@@ -1,37 +1,41 @@
 package org.cts.aircanada.login;
 
 public class CourseDetails {
-	
+
 	private void javaCourse() {
 		System.out.println("Java");
 
 	}
-	
+
 	private void seleniumCourse() {
 		System.out.println("Selenium");
 
 	}
-	
+
 	private void oracleCourse() {
 		System.out.println("Oracle");
 
 	}
-	
+
 	public static void main(String[] args) {
-		
-		//Create an object --- ClassName obj/RefName = new ClassName();
-		
+
+		// Create an object --- ClassName obj/RefName = new ClassName();
+
 		CourseDetails cou = new CourseDetails();
-		
-		//Call the methods -- ObjName.MethodName();
-		
+
+		// Call the methods -- ObjName.MethodName();
+
 		cou.javaCourse();
 		cou.oracleCourse();
 		cou.seleniumCourse();
-		
-		
+
+		System.out.println("Added new Code");
+		System.out.println("Added new Code");
+
+		System.out.println("Added new Code");
+
+		System.out.println("Added new Code");
+
 	}
-	
 
 }
-
